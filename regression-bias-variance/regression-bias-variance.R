c75c7ca09a6ad3ad3f615ed7b52a974dbd3955cd
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-04-01 17:29:52 +1300

    restruucture slightly exit()

--- regression-bias-variance/regression-bias-variance.R
+++ regression-bias-variance/regression-bias-variance.R
@@ -77,3 +77,39 @@
 # High p value, results in correlation. Need to really think about it before confirming causation
 
 
+setwd("/Users/tarineccleston/Documents/Masters/STATS 765/statistical-learning-for-DS/regression-bias-variance")
+
+aq_data = data.frame(airquality)
+
+# we want to model effect of solar radiation on temperature
+# we want to take temperature and wind into consideration
+# plot ozone as explained by solar radiation, given each combination of temp and wind
+coplot(Ozone ~ Solar.R|Temp + Wind, data = aq_data, n = 4)
+
+# Interaction Terms:
+# as shown from the coplot, the relationship between the solar radiation and ozone
+# depends on the temperature and wind
+# the figure is a clear indication that we have an interaction term
+
+# EOV Assumption:
+# scatter appears to increase (variance) as ozone increase, use poisson distribution
+# to model behavior
+
+# use log scale
+# seems to have very little interaction between solar and temp and solar and wind
+coplot(log(Ozone) ~ Solar.R|Temp + Wind, data = aq_data, n = 4)
+
+# now, consider interaction between solar radiation and temperature, and solar
+# radiation and wind on the original scale
+aq_fit = lm(Ozone ~ Solar.R * (Temp + Wind), data = aq_data)
+summary(aq_fit)
+
+# p-values for interaction terms seem statistically significant interaction terms
+# adjusted R-square is 0.64. This model explains 65% of the variability
+
+# now compare interaction terms when using log scale.
+aq_fit = lm(log(Ozone) ~ Solar.R * (Temp + Wind), data = aq_data)
+summary(aq_fit)
+
+# high p-values for the interaction term confirm that they are not significant at 
+# explaining our data
